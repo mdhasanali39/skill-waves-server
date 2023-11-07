81fd54b48fe9afc58094dc1d46837275549414f9
mdhasanali39 <contact.hasankhanyt@gmail.com>
2023-11-07 14:48:25 +0600

    index.js file updated with delete job api for delete specific job by id

--- index.js
+++ index.js
@@ -75,7 +75,14 @@ async function run() {
       res.send(result)
     })
 
-    
+    // delete job 
+    app.delete("/api/v1/job/delete-job/:id", async(req, res)=>{
+      const id = req.params.id;
+      // console.log(id);
+      const query = {_id: new ObjectId(id)}
+      const result = await jobsCollection.deleteOne(query)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
